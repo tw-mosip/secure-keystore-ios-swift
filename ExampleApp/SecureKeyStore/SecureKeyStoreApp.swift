7b16f1bfa1a051031537f09e02155f2888fb15e4
Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>
2024-09-10 13:51:22 +0530

    [INJIMOB-1874] Add  example app
    Signed-off-by: Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>

---
+++ ExampleApp/SecureKeyStore/SecureKeyStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecureKeyStoreApp.swift
+//  SecureKeyStore
+//
+//  Created by Abhishek Paul on 09/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SecureKeyStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
