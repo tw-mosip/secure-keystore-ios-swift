0b73f3e520bcde43cfd4fb460333a19c581599ca
Kaushik Gupta <kausgpt97@gmail.com>
2026-02-25 15:15:40 +0530

    INJIMOB-3692 : New api call for adaptive biometric feature (#24)
    * INJIMOB-3692 : New api call for adaptive biometric feature
    
    Signed-off-by: Kaushik Gupta <kausgpt97@gmail.com>
    
    * INJIMOB-3692: Refactor logic for hardware capability
    
    Signed-off-by: Kaushikgupta469 <kausgpt97@gmail.com>
    
    * INJIMOB-3692: Refractoring and cleanup code file
    
    Signed-off-by: Kaushikgupta469 <kausgpt97@gmail.com>
    
    * INJIMOB-3692: Renamed api call and Refractoring
    
    Signed-off-by: Kaushikgupta469 <kausgpt97@gmail.com>
    
    ---------
    
    Signed-off-by: Kaushik Gupta <kausgpt97@gmail.com>
    Signed-off-by: Kaushikgupta469 <kausgpt97@gmail.com>

--- Sources/securekeystore/Biometrics/BiometricsProtocol.swift
+++ Sources/securekeystore/Biometrics/BiometricsProtocol.swift
@@ -3,4 +3,5 @@ import LocalAuthentication
 public protocol BiometricsProtocol {
     func authenticateUser(keyType: String, reason: String, completion: @escaping (Bool, Error?) -> Void)
     func hasBiometricsEnabled()->Bool
+    func getAvailableBiometricType()->String
 }
--- Sources/securekeystore/SecureKeystoreProtocol.swift
+++ Sources/securekeystore/SecureKeystoreProtocol.swift
@@ -17,6 +17,8 @@ public protocol SecureKeystoreProtocol {
     
     func hasBiometricsEnabled() -> Bool
     
+    func getAvailableBiometricType() -> String
+    
     func updatePopup(title: String, desc: String)
     
     func generateKey(alias: String, authRequired: Bool, authTimeout: Int32, onSuccess: @escaping (Bool) -> Void, onFailure: @escaping(_ code: String, _ message: String) -> Void)
